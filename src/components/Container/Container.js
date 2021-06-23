fc1df2746546d92b1ca8547c6b73e21229730280
Piotr Kosiński <kosinskipiotr.98@gmail.com>
2021-06-23 17:59:59 +0200

    Add MainLayout, Header, Home, App components & Info subpage using react-router

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,13 +2,18 @@ import React from 'react';
 import styles from './Container.scss';
 import PropTypes from 'prop-types';
 
-const Container = (props) => (
-  <div className={styles.component}>{props.children}</div>
-);
-  
-Container.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-  
+class Container extends React.Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  }
+  render() {
+    const { children } = this.props;
+    return (
+      <div className={styles.component}>
+        {children}
+      </div>
+    );
+  }
+}
 
 export default Container;
